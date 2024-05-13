b4adfc7e85913d4697f02089eb3066e7fcea3eae
Rolin Blake <rolinmblake@gmail.com>
2024-05-13 06:23:55 -0500

    renaming functions and handling different OS

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@ use std::io::prelude::*;
 //use std::io::{BufRead, BufReader};
 use std::process::{Command/*, Stdio*/};
 
-fn call_matplotlib(data: Vec<f64>) {
+fn call_matplotlib_simple(data: Vec<f64>) {
   let python_str = format!("import matplotlib.pyplot as plt\nif __name__ == \"__main__\":\n\tdata = {:?}\n\tprint(\"\\n(main.py) data: Vec<f64> from src/main.rs =\")\n\tprint(data)\n\tplt.plot(data)\n\tplt.xlabel(\"Index\")\n\tplt.ylabel(\"Value\")\n\tplt.ylabel(\"Simple Plot\")\n\tplt.show()", data);
   let mut file = File::create("src/main.py").expect("(main.rs) :: call_matplotlib() Unable to write python_str to src/main.py");
   file.write_all(python_str.as_bytes()).expect("(main.rs) :: call_matplotlib() Unable to write python_str to src/main.py");
-  let output = Command::new("python").arg("src/main.py").output().expect("(main.rs) :: call_matplotlib() Failed to execute src/main.py");
-  println!("(main.rs) :: call_matplotlib() Output from src/main.py ~~~\n{}\n~~~", String::from_utf8_lossy(&output.stdout));
+  if std::env::consts::OS == "windows" {
+    let pycmd = Command::new("python").arg("src/main.py").output().expect("(main.rs) :: call_matplotlib() Failed to execute src/main.py");
+    println!("(main.rs) :: call_matplotlib() Output from src/main.py ~~~\n{}\n~~~", String::from_utf8_lossy(&pycmd.stdout));
+  } else {
+    let pycmd = Command::new("python3").arg("src/main.py").output().expect("(main.rs) :: call_matplotlib() Failed to execute src/main.py");
+    println!("(main.rs) :: call_matplotlib() Output from src/main.py ~~~\n{}\n~~~", String::from_utf8_lossy(&pycmd.stdout));
+  }
 }
 
 /*fn open_python_cli() {
@@ -25,6 +30,6 @@ fn call_matplotlib(data: Vec<f64>) {
 fn main() {
   let data: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0];
   println!("(main.rs) :: main() data: Vec<f64> = {:?}\n", data);
-  call_matplotlib(data);
+  call_matplotlib_simple(data);
   //open_python_cli();
 }
