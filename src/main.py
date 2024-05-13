58428c6f639f015e16c4597330eb380eb802e58e
Rolin Blake <rolinmblake@gmail.com>
2024-05-13 06:02:02 -0500

    now using matplotlib from rust; somehow matplot lib shows the plot before the other print statements?

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+import matplotlib.pyplot as plt
 if __name__ == "__main__":
 	floats = [1.0, 2.0, 3.0, 4.0, 5.0]
 	print("\n(main.py) floats: Vec<f64> from src/main.rs =")
 	print(floats)
+	fig = plt.plot(floats)
+	plt.xlabel("Index")
+	plt.ylabel("Value")
+	plt.ylabel("Simple Plot")
+	plt.show()
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{BufRead, BufReader};
-use std::process::{Command, Stdio};
+//use std::io::{BufRead, BufReader};
+use std::process::{Command/*, Stdio*/};
 
 fn main() {
   let floats: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0];
   println!("(main.rs) floats: Vec<f64> = {:?}\n", floats);
-  let python_str = format!("if __name__ == \"__main__\":\n\tfloats = {:?}\n\tprint(\"\\n(main.py) floats: Vec<f64> from src/main.rs =\")\n\tprint(floats)", floats);
+  let python_str = format!("import matplotlib.pyplot as plt\nif __name__ == \"__main__\":\n\tfloats = {:?}\n\tprint(\"\\n(main.py) floats: Vec<f64> from src/main.rs =\")\n\tprint(floats)\n\tfig = plt.plot(floats)\n\tplt.xlabel(\"Index\")\n\tplt.ylabel(\"Value\")\n\tplt.ylabel(\"Simple Plot\")\n\tplt.show()", floats);
   let mut file = File::create("src/main.py").expect("Unable to create src/main.py");
   file.write_all(python_str.as_bytes()).expect("(main.rs) Unable to write python_str to src/main.py");
   let output = Command::new("python").arg("src/main.py").output().expect("(main.rs) Failed to execute src/main.py");
